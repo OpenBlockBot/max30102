b25293e0993e8300db5db6bc8fa97b4192b82acf
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:19:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# MAX30102 Sensor
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/max30102?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-sensor-lightgrey.svg)
+
+Blood oxygen and heart rate detection module based on MAX30102.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,79 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'max30102.name': 'MAX30102 Sensor',
+            'max30102.description': 'Blood oxygen and heart rate detection module based on MAX30102.'
+        },
+        'ru': {
+            'max30102.name': 'MAX30102 Sensor',
+            'max30102.description': 'Blood oxygen and heart rate detection module based on MAX30102.'
+        },
+        'zh-cn': {
+            'max30102.name': 'MAX30102 Sensor',
+            'max30102.description': 'Blood oxygen and heart rate detection module based on MAX30102.'
+        },
+        'zh-tw': {
+            'max30102.name': 'MAX30102 Sensor',
+            'max30102.description': 'Blood oxygen and heart rate detection module based on MAX30102.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'MAX30102_CATEGORY': 'MAX30102',
+            'MAX30102_INIT': 'init max30102',
+            'MAX30102_GETDATA': 'max30102 get date',
+            'MAX30102_IRVALUE': 'max30102 IR value',
+            'MAX30102_HEARTBEAT': 'max30102 heart beat value per minite',
+            'MAX30102_ISTOUCHED': 'max30102 is touched?'
+        },
+        'ru': {
+            'MAX30102_CATEGORY': 'MAX30102',
+            'MAX30102_INIT': 'init max30102',
+            'MAX30102_GETDATA': 'max30102 get date',
+            'MAX30102_IRVALUE': 'max30102 IR value',
+            'MAX30102_HEARTBEAT': 'max30102 heart beat value per minite',
+            'MAX30102_ISTOUCHED': 'max30102 is touched?'
+        },
+        'zh-cn': {
+            'MAX30102_CATEGORY': 'MAX30102',
+            'MAX30102_INIT': 'init max30102',
+            'MAX30102_GETDATA': 'max30102 get date',
+            'MAX30102_IRVALUE': 'max30102 IR value',
+            'MAX30102_HEARTBEAT': 'max30102 heart beat value per minite',
+            'MAX30102_ISTOUCHED': 'max30102 is touched?'
+        },
+        'zh-tw': {
+            'MAX30102_CATEGORY': 'MAX30102',
+            'MAX30102_INIT': 'init max30102',
+            'MAX30102_GETDATA': 'max30102 get date',
+            'MAX30102_IRVALUE': 'max30102 IR value',
+            'MAX30102_HEARTBEAT': 'max30102 heart beat value per minite',
+            'MAX30102_ISTOUCHED': 'max30102 is touched?'
+        }
+    }
+};
